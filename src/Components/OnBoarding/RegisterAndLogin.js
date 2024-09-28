477067cf72e384b3ecb9f3ba1deac3e752fd9b8d
karishmaMohammed <karishmamohammed43@gmail.com>
2024-09-28 10:59:59 +0530

    rfce code

---
+++ src/Components/OnBoarding/RegisterAndLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterAndLogin() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterAndLogin
---
+++ src/Components/TaskManagement/TaskManagement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TaskManagement() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TaskManagement
